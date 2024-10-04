b729edd46aa80008989914db68f36f975852d68b
Lars de Kooter <de.kooter.lars@gmail.com>
2024-10-04 19:34:34 +0200

    Change load-commands way

--- src/Commands/load-commands.ts
+++ src/Commands/load-commands.ts
@@ -1,9 +1,4 @@
-import {
-  REST,
-  Routes,
-  SlashCommandBuilder,
-  InteractionReplyOptions,
-} from "discord.js";
+import { SlashCommandBuilder } from "discord.js";
 import Command from "../Structures/Command.js";
 import * as fs from "fs";
 
@@ -12,7 +7,7 @@ export default new Command({
     .setName("load-commands")
     .setDescription("Reload Commands"),
   async run(interaction, client) {
-    await interaction.deferReply();
+    await interaction.deferReply({ ephemeral: true });
 
     // Load chat-input commands
     fs.readdirSync("./js/Commands")
@@ -31,22 +26,14 @@ export default new Command({
         client.contextMenuCommands.set(command.data.name, command);
       });
 
-    const rest = new REST().setToken(process.env.DISCORD_TOKEN);
-    await rest.put(
-      Routes.applicationGuildCommands(client.user.id, "950680035411501056"),
-      {
-        body: [
-          ...client.commands.map((command) => command.data.toJSON()),
-          ...client.contextMenuCommands.map((command) => command.data.toJSON()),
-        ],
-      }
+    await client.application.commands.set(
+      client.commands.map((command) => command.data.toJSON())
     );
 
     await interaction.editReply({
       content: `Loaded ${
         client.commands.size + client.contextMenuCommands.size
       } commands`,
-      ephemeral: true,
-    } as InteractionReplyOptions);
+    });
   },
 });
